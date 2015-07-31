1330ff917eb554cd715bfd172580db9c89b82dc1
Nutchalum Kitpongsai <nutch.kps@gmail.com>
2015-07-31 20:46:37 +0700

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,5 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('asset/scss/main.scss', 'public/css/main.css');
 });
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CNC Laboratory</title>
+	<link href="{{ URL::to('materialize/css/materialize.min.css') }}" rel="stylesheet" type="text/css">
+  	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+	<nav>
+		<div class="container">
+			<div class="nav-wrapper navigator">
+				<a href="#" class="left brand-logo">Logo</a>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a href="#">Showcase</a></li>
+					<li><a href="#">News</a></li>
+					<li><a href="#">Activity</a></li>
+					<li><a href="#">Member</a></li>
+					<li><a href="#">Contact Us</a></li>
+					<li><a href="#">Recruit</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="{{ URL::to('materialize/js/materialize.min.js') }}"></script>
+</body>
+</html>
