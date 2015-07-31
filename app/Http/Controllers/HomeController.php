88a13ea057864fe3a5bcd9f56ce9c3dd605d79fb
Nutchalum Kitpongsai <nutch.kps@gmail.com>
2015-07-31 21:22:08 +0700

    extends usage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('layouts.main');
+        return view('home');
     }
 
     /**
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,20 +3,22 @@
 <head>
 	<title>CNC Laboratory</title>
 	<link href="{{ URL::to('materialize/css/materialize.min.css') }}" rel="stylesheet" type="text/css">
-  	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
 	<nav>
 		<div class="container">
 			<div class="nav-wrapper navigator">
 				<a href="#" class="left brand-logo">Logo</a>
-<ul class="right hide-on-med-and-down">
-        <li><a href="sass.html"><i class="material-icons left">search</i>Link with Left Icon</a></li>
-        <li><a href="badges.html"><i class="material-icons right">view_module</i>Link with Right Icon</a></li>
-      </ul>
+				<ul class="right hide-on-med-and-down">
+					<li><a href="sass.html"><i class="material-icons left">search</i>Link with Left Icon</a></li>
+					<li><a href="badges.html"><i class="material-icons right">view_module</i>Link with Right Icon</a></li>
+				</ul>
 			</div>
 		</div>
 	</nav>
+
+	@yield('content')
 
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 	<script type="text/javascript" src="{{ URL::to('materialize/js/materialize.min.js') }}"></script>
