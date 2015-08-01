5c149151b1d87b3a8fc99166d885a661e51debcc
unknown <p.jirawattanakitja@MW7GY2K1DSL68B.dir.svc.accenture.com>
2015-08-01 16:16:36 +0700

    Add thumbnail pictures

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -36,7 +36,7 @@
 					<span class="card-title activator grey-text text-darken-4">Usa</span>
 					<div class="card-title activator grey-text text-darken-4 surname">Sammapun</div>
 
-					<p><a href="#">Profile</a></p>
+					<p><a href="{{ url("member/show") }}">Profile</a></p>
 				</div>
 				<div class="card-reveal">
 					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
@@ -48,17 +48,18 @@
 
 
 		<div class="col s12  m4">
+
 			<div class="card">
 				<div class="card-image waves-effect waves-block waves-light">
 					<div class="crop">
-					<img class="activator" src={{ URL::to('/') }}/images/profile2.jpg>
+					<img class="activator" src={{ URL::to('/') }}/images/profile3.jpg>
 					</div>
 				</div>
 				<div class="card-content">
-					<span class="card-title activator grey-text text-darken-4">Usa</span>
-					<div class="card-title activator grey-text text-darken-4 surname">Sammapun</div>
+					<span class="card-title activator grey-text text-darken-4">Chakrit</span>
+					<div class="card-title activator grey-text text-darken-4 surname">Watcharopas</div>
 
-					<p><a href="#">Profile</a></p>
+					<p><a href="{{ url("member/show") }}">Profile</a></p>
 				</div>
 				<div class="card-reveal">
 					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
@@ -69,16 +70,17 @@
 
 		<div class="col s12  m4">
 			<div class="card">
+
 				<div class="card-image waves-effect waves-block waves-light">
 					<div class="crop">
-					<img class="activator" src={{ URL::to('/') }}/images/profile2.jpg>
+					<img class="activator" src={{ URL::to('/') }}/images/profile4.jpg>
 					</div>
 				</div>
 				<div class="card-content">
-					<span class="card-title activator grey-text text-darken-4">Usa</span>
-					<div class="card-title activator grey-text text-darken-4 surname">Sammapun</div>
+					<span class="card-title activator grey-text text-darken-4">Sukumal</span>
+					<div class="card-title activator grey-text text-darken-4 surname">Kitisin</div>
 
-					<p><a href="#">Profile</a></p>
+					<p><a href="{{ url("member/show") }}">Profile</a></p>
 				</div>
 				<div class="card-reveal">
 					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
@@ -99,14 +101,14 @@
 			<div class="card">
 				<div class="card-image waves-effect waves-block waves-light">
 					<div class="crop">
-					<img class="activator" src={{ URL::to('/') }}/images/profile2.jpg>
+					<img class="activator" src={{ URL::to('/') }}/images/profile.jpg>
 					</div>
 				</div>
 				<div class="card-content">
-					<span class="card-title activator grey-text text-darken-4">Usa</span>
-					<div class="card-title activator grey-text text-darken-4 surname">Sammapun</div>
+					<span class="card-title activator grey-text text-darken-4">Promboon</span>
+					<div class="card-title activator grey-text text-darken-4 surname">Jirawattanakitja</div>
 
-					<p><a href="#">Profile</a></p>
+					<p><a href="{{ url("member/show") }}">Profile</a></p>
 				</div>
 				<div class="card-reveal">
 					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
@@ -121,14 +123,14 @@
 			<div class="card">
 				<div class="card-image waves-effect waves-block waves-light">
 					<div class="crop">
-					<img class="activator" src={{ URL::to('/') }}/images/profile2.jpg>
+					<img class="activator" src={{ URL::to('/') }}/images/profile5.jpg>
 					</div>
 				</div>
 				<div class="card-content">
-					<span class="card-title activator grey-text text-darken-4">Usa</span>
-					<div class="card-title activator grey-text text-darken-4 surname">Sammapun</div>
+					<span class="card-title activator grey-text text-darken-4">Panudej</span>
+					<div class="card-title activator grey-text text-darken-4 surname">Pamornsiritragul</div>
 
-					<p><a href="#">Profile</a></p>
+					<p><a href="{{ url("member/show") }}">Profile</a></p>
 				</div>
 				<div class="card-reveal">
 					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
@@ -137,18 +139,19 @@
 			</div>
 		</div>
 
+
 		<div class="col s12  m4">
 			<div class="card">
 				<div class="card-image waves-effect waves-block waves-light">
 					<div class="crop">
-					<img class="activator" src={{ URL::to('/') }}/images/profile2.jpg>
+					<img class="activator" src={{ URL::to('/') }}/images/profile6.jpg>
 					</div>
 				</div>
 				<div class="card-content">
-					<span class="card-title activator grey-text text-darken-4">Usa</span>
-					<div class="card-title activator grey-text text-darken-4 surname">Sammapun</div>
+					<span class="card-title activator grey-text text-darken-4">Nutchalum</span>
+					<div class="card-title activator grey-text text-darken-4 surname">Kitpongsai</div>
 
-					<p><a href="#">Profile</a></p>
+					<p><a href="{{ url("member/show") }}">Profile</a></p>
 				</div>
 				<div class="card-reveal">
 					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
